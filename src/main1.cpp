e000b55c324fbe33ec6922670e0059facc7699cd
jacdaniel <jacques.daniel.fr@gmail.com>
2022-04-10 11:34:38 +0200

    master encore un (2)

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,9 +1,11 @@
 
+
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
   fprintf(stderr, "ok\n");
   fprintf(stderr, "on continue sur master"); 
+  fprintf(stderr, "encore un");
   return 0;
 }
