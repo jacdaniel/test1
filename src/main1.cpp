8b88b8397afe8a7b5b627e2dcbb26c05483ce4d8
jacdaniel <jacques.daniel.fr@gmail.com>
2022-04-10 11:40:18 +0200

    un autre commit sur master (3)

--- src/main1.cpp
+++ src/main1.cpp
@@ -7,5 +7,6 @@ int main(int argc, char **argv)
   fprintf(stderr, "ok\n");
   fprintf(stderr, "on continue sur master"); 
   fprintf(stderr, "encore un");
+  fprintf(stderr, "et un de plus sur master")
   return 0;
 }
